2f0d78f3b930bfa0d9dbaf3532d581d9d5c08cf3
rb <rafael.bonanno@gmail.com>
2023-01-06 11:15:27 +0100

    changes on front page

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -4,5 +4,20 @@ export default class extends Controller {
   connect() {
     this.element.textContent = "Hello World!"
   }
-  
 }
+  function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add(" active");
+      } else {
+        reveals[i].classList.remove(" active");
+      }
+    }
+  }
+  window.addEventListener("scroll", reveal);
